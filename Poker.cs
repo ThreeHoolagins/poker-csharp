4fb57911610b8663cb03a41bc0e4f2762efd8423
Caleb C <cbcunningham20@gmail.com>
2020-08-29 17:23:45 -0500

    Built out card and deck functionality

--- Poker.cs
+++ Poker.cs
@@ -66,12 +66,49 @@ class Card {
     suit = inputSuit;
     card = inputCard;
   }
+
+  void printCard(){
+    string print = $"{card} of {suit}";
+    Console.WriteLine(print);
+  }
 }
 
 class Deck {
+  static string[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+  static string[] cards = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
   Card[] currentDeck;
+  bool[][] hasCard = new bool[4][13];
 
   new Deck(int numberOfCards = 52, bool hasJokers = false) {
-    // this methoud makes a deck but only with
+    // this methoud makes a deck but only with enough cards to play the game to save game space
+    // Defult Paramaters make a standard set of cards
+
+    //Initalize bool array to make sure we are safe on duplicates
+    foreach (card in hasCard) {
+      card = false;
+    }
+    currentDeck = new Card[numberOfCards];
+
+    // Pick up random cards
+    var rng = new Random();
+    for (int i = 0; i < numberOfCards; i++) {
+      int randSuit = rng.next(0,4);
+      int randCard = rng.next(0,13);
+
+      // Confirm that we don't have duplicates, loop until we don't again
+      while(hasCard[randSuit][randCard]) {
+        int randSuit = rng.next(0,4);
+        int randCard = rng.next(0,13);
+      }
+
+      currentDeck[i] = new Card(suits[randSuit], cards[randCard]);
+      hasCard[randSuit][randCard] = false;
+    }
+  }
+
+  void printDeck() {
+    foreach (card in currentDeck) {
+      card.PrintCard;
+    }
   }
 }
